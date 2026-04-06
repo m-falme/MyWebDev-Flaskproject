d76578fd64c21c1a277c3cad5dd1740b78bb48bc
Mark Mburu <mburumark096@gmail.com>
2026-04-06 03:16:47 +0300

    fixed an small error on flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -39,7 +39,7 @@ def register():
 
     return render_template("register.html", title='Register', form=form)
 
-@app.route("/login", methods=['GET', 'POST']))
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     return render_template("login.html", title='Login', form=form)
