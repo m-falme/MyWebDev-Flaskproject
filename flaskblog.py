2e14c4bad0d4b315268f9ee66547a16f3545e288
Mark Mburu <mburumark096@gmail.com>
2026-04-08 05:03:15 +0300

    updated register and login

--- flaskblog.py
+++ flaskblog.py
@@ -34,7 +34,7 @@ def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
-        flash(f'Account created for{form.username.data}!', 'success')
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
 
     return render_template("register.html", title='Register', form=form)
@@ -42,8 +42,13 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful. Please check email and password', 'danger')
     return render_template("login.html", title='Login', form=form)
-
 if __name__ == "__main__":
     app.run(debug=True)    
 
