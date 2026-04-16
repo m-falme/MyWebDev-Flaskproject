f9d2e78527f4778335c8f3b396354f4461964424
Mark Mburu <mburumark096@gmail.com>
2026-04-16 04:33:40 +0300

    set up sql and updated files

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,29 +1,7 @@
-from flask import Flask, render_template, url_for, flash, redirect
-from flask_sqlalchemy import SQLAlchemy
-from form import RegistrationForm, LoginForm
-
-app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'c92f82efca21b59c1f48d3d80ebd9566'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    username = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
-    password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='author', lazy=True)
-    def __ref__(self):
-        return f"User('{self.username}', '{self.email}, '{self.image_file}')"
-
-
-class Post(db.Model):
-
-
-
+from flask import render_template, url_for, flash, redirect
+from flaskblog import app, db
+from flaskblog.models import User
+from flaskblog.forms import RegistrationForm, LoginForm
 
 
 posts = [
@@ -39,23 +17,34 @@ posts = [
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     }
-
 ]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html", posts=posts)
 
+
 @app.route("/about")
 def about():
-    return render_template("about.html", title='About')    
+    return render_template("about.html", title='About')
+
 
 @app.route("/register", methods=['GET', 'POST'])
-def register(): 
+def register():
     form = RegistrationForm()
 
     if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=form.password.data
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
 
@@ -64,13 +53,12 @@ def register():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
             flash('Login unsuccessful. Please check email and password', 'danger')
-    return render_template("login.html", title='Login', form=form)
-if __name__ == "__main__":
-    app.run(debug=True)    
 
+    return render_template("login.html", title='Login', form=form)
